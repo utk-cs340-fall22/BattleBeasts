4e03c6dce8178a15dbaf260a0beb4594943d78de
Christopher Canaday <chrislcanaday@gmail.com>
2022-09-27 14:09:48 -0400

    #1 #2 Linked Title to Main Menu. Exit button on Main Menu now closes the program.

---
+++ Menus/MainMenu.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public class MainMenu : CanvasLayer
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+	
+	private void _on_Exit_pressed()
+	{
+		// Replace with function body.
+		GetTree().Quit();
+	}
+
+
+	private void _on_Settings_pressed()
+	{
+		// Replace with function body.
+	}
+
+
+	private void _on_Play_pressed()
+	{
+		// Replace with function body.
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
