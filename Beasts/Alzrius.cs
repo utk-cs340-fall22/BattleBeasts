b0418ed4bded4f5e6efae79c8f4b91552f63a59f
stephhill77 <shill77@vols.utk.edu>
2022-10-06 16:52:47 -0400

    Added Alzrius sprite and cs file

--- Beasts/Alzrius.cs
+++ Beasts/Alzrius.cs
@@ -1,21 +1,18 @@
 using Godot;
 using System;
 
-public class Beasts : Node2D
+public class Alzrius : AnimatedSprite
 {
-	private int hp = 100;
-	private int attack = 100;
-	private int defence = 100;
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Hide();
+		
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
- 	public override void _Process(float delta)
- 	{
-	  
-  	}
+	public override void _Process(float delta)
+	{
+		
+	}
 }
