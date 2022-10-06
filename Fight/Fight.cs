eee57456587c497d1be64f565f60e1dd87597979
Colin Canonaco <colin.canonaco@gmail.com>
2022-10-06 00:12:35 -0400

    player fighter can attack opponent fighter and reduce opponent fighter's health using attack 0 (button 1)

--- Fight/Fight.cs
+++ Fight/Fight.cs
@@ -7,26 +7,40 @@ public class Fight : Node
   [Export]
   public PackedScene Fighter;
 #pragma warning restore 649
+
+  int playerMaxHealth, opponentMaxHealth;
+  Fighter player, opponent;
   
   // Called when the node enters the scene tree for the first time.
-  public override void _Ready()
-  {
+  public override void _Ready() {
 	int[] attackSet = new int[4];
+	playerMaxHealth = 100;
+	opponentMaxHealth = 132;
 
 	// initialize player character
-	Fighter player = (Fighter)Fighter.Instance();
+	player = (Fighter)Fighter.Instance();
 	attackSet = new int[] {1, 2, 3, 4};
-	player.Init("player", attackSet);
+	player.Init("player", attackSet, playerMaxHealth);
 
 	// initialize opponent character
-	Fighter opponent = (Fighter)Fighter.Instance();
+	opponent = (Fighter)Fighter.Instance();
 	attackSet = new int[] {2, 4, 3, 1};
-	opponent.Init("opponent", attackSet);
+	opponent.Init("opponent", attackSet, opponentMaxHealth);
+
+	// debug
+	GD.Print("oppoonent health: ", opponent.Reduce_Health(0));
   }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+  /*
+  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta)
+  {
+	// stuff
+  }
+  */
+
+  private void _on_B1_pressed()
+  {
+	GD.Print("opponent health remaining: ", opponent.Reduce_Health(player.Get_Attack_Strength(0)));
+  }
 }
--- Fight/Fighter.cs
+++ Fight/Fighter.cs
@@ -3,31 +3,64 @@ using System;
 
 public class Fighter : Sprite
 {
-  string controller;
-  int health, maxHealth;
-  int[] attackStats = new int[4]; // attack damage
-  
-  // initialize as player/opponent, load stats
-  public void Init(string controller, int[] attackSet) {
-    int i;
+  string controller; // fighter is controlled by player or the opponent (computer)
+  int health; // current health
+  int maxHealth; // max health
+  int[] attackStats = new int[4]; // attack damage for the 4 different attacks
 
-    // load attack identifiers
-    this.controller = controller;
-    for (i = 0; i < attackSet.Length; i++) {
-      attackStats[i] = attackSet[i];
-    }
+  /*
+  initialize as player or opponent and load stats
+  controller: what controls this fighter, "player" or "opponent"  
+  attackSet: array of attack IDs indicating what attacks this fighter has
+  maxHealth: maximum health value
+  */
+  public void Init(string controller, int[] attackSet, int maxHealth) {
+	int i;
 
-    // debugging
-    GD.Print("controller: ", controller);
-    for (i = 0; i < attackSet.Length; i++) {
-      GD.Print("attack", i, ": ", attackSet[i]);
-    }
+	// load attack identifiers
+	this.controller = controller;
+	this.maxHealth = maxHealth;
+  health = maxHealth;
+	for (i = 0; i < attackSet.Length; i++) {
+	  attackStats[i] = attackSet[i];
+	}
+
+	// debugging
+	GD.Print("controller: ", controller);
+	for (i = 0; i < attackSet.Length; i++) {
+	  GD.Print("attack", i, ": ", attackSet[i]);
+	}
+  }
+
+  /*
+  return how much damage an attack does
+  attackID: id of the attack
+  return: damage value of attack
+  */
+  public int Get_Attack_Strength(int attackID) {
+	return attackStats[attackID];
+  }
+
+  /*
+  reduce this fighter's health by a given amount
+  damage: how much to reduce the fighter's health by
+  return: resulting health value
+  */
+  public int Reduce_Health(int damage) {
+	if (damage >= health) {
+	  health = 0;
+	}
+	else {
+	  health -= damage;
+	}
+
+	return health;
   }
 
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
-    
+	
   }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
