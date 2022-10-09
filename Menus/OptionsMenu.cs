d7efdf705f69799a8a83086628f7afc36aff1d1f
Christopher Canaday <chrislcanaday@gmail.com>
2022-10-09 14:09:51 -0400

    #9 Reworked the PauseMenu to allow for easier use and more intuitive functionality

--- Menus/OptionsMenu.cs
+++ Menus/OptionsMenu.cs
@@ -10,6 +10,7 @@ public class OptionsMenu : CanvasLayer
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
+    
   }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
