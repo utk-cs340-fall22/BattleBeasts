54e5376c6bc50da8d6e3d4b46fad9be7fa844b3d
Jake Looney <jlooney314@gmail.com>
2022-10-10 18:37:01 -0400

    #13 team building scene (unlinked)

--- Menus/TitleMenu.cs
+++ Menus/TitleMenu.cs
@@ -4,27 +4,27 @@ using System;
 public class TitleMenu : CanvasLayer
 {
   // Declare member variables here. Examples:
-  private AnimationPlayer _titleAnim;
-
+  private AnimationPlayer titleAnim;
+  
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
-  _titleAnim = GetNode<AnimationPlayer>("AnimationPlayer");
+    titleAnim = GetNode<AnimationPlayer>("AnimationPlayer");
   
-  _titleAnim.Play("test");
+    titleAnim.Play("test");
   }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
   {
-  if( _titleAnim.IsPlaying() == false ){
-  _titleAnim.Play("test");
-  } 
+    if( titleAnim.IsPlaying() == false ){
+      titleAnim.Play("test");
+    } 
   }
 
   private void _on_Button_pressed()
   {
-  GetTree().ChangeScene("res://Menus/MainMenu.tscn");
+    GetTree().ChangeScene("res://Menus/MainMenu.tscn");
   
   }
 }
