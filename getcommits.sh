7cffb9382ab764da48f9f411ffda3b44b0e84972
Christopher Canaday <chrislcanaday@gmail.com>
2022-10-11 15:17:23 -0400

    Updated shellscript to handle 1 word names

--- getcommits.sh
+++ getcommits.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if [ $# != 2 ]; then
-  echo 'usage: sh getcommits.sh First-Name Last-Name' >&2
-  exit 1
-fi
-
-#read name
-str="$1"
-str=\"$str\"
-str2="$2"
-str2=\"$str\"
 rm commits.txt
 for i in *
 do
@@ -19,16 +9,26 @@ do
         for dir2 in *; do
             if [ "${dir2: -3}" == ".cs" ] 
             then
-            echo "git blame -w $i/$dir2 | grep -e $1 -e $2 >> ../commits.txt"
-            git blame -w "$dir2" | grep -e "$1" -e "$2" >> ../commits.txt
+	    if [ $# == 2 ]; then  
+		echo "git blame -w $i/$dir2 | grep -e $1 -e $2 >> ../commits.txt"
+		git blame -w "$dir2" | grep -e $1 -e $2 >> ../commits.txt	
+      	    else
+		echo "git blame -w $i/$dir2 | grep $1 >> ../commits.txt"
+		git blame -w "$dir2" | grep $1 >> ../commits.txt
+      	    fi
             fi
         done
         cd ..
     else
     if [ "${i: -3}" == ".cs" ]
     then
-      echo "git blame -w $i | grep -e $1 -e $2 >> commits.txt"
-      git blame -w $i | grep -e $str -e $str2 >> commits.txt
+      if [ $# == 2 ]; then
+        echo "git blame -w $i | grep -e $1 -e $2 >> commits.txt"
+	git blame -w $i | grep -e $1 -e $2 >> commits.txt
+      else
+	echo "git blame -w $i | grep $1 >> commits.txt" 
+	git blame -w $i | grep $1 >> commits.txt
+      fi
     fi
     fi
 done
