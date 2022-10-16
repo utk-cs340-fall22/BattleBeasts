17c7a4154be82f9af7f7f010e985c877cda180d9
Colin Canonaco <colin.canonaco@gmail.com>
2022-10-16 18:12:40 -0400

    player and opponent take turns attacking each other. ai chooses attacks randomly but method for choosing exists for easy additions to its decision making

--- Fight/Fight.cs
+++ Fight/Fight.cs
@@ -8,7 +8,7 @@ public class Fight : Node
   public PackedScene Fighter;
 #pragma warning restore 649
 
-  int playerMaxHealth, opponentMaxHealth;
+  int playerMaxHealth, opponentMaxHealth, isPlayerTurn, aiAttackChoice;
   Fighter player, opponent;
   Texture playerTexture, opponentTexture;
   
@@ -17,6 +17,8 @@ public class Fight : Node
     int[] attackSet = new int[4];
     playerMaxHealth = 100;
     opponentMaxHealth = 132;
+    isPlayerTurn = 1;
+    GD.Randomize();
 
     /* Initialize player character */
 
@@ -26,7 +28,7 @@ public class Fight : Node
     player.GetNode<Sprite>("Texture").Texture = playerTexture;
     player.Position = new Vector2(190, 280);
     player.Scale = new Vector2(6, 6);
-    attackSet = new int[] {1, 2, 13, 50};
+    attackSet = new int[] {1, 2, 13, 1000};
     player.Init("player", attackSet, playerMaxHealth);
 
     /* Initialize opponent character */
@@ -37,41 +39,85 @@ public class Fight : Node
     opponent.GetNode<Sprite>("Texture").Texture = opponentTexture;
     opponent.Position = new Vector2(850, 170);
     opponent.Scale = new Vector2(6, 6);
-    attackSet = new int[] {2, 4, 3, 1};
+    attackSet = new int[] {1, 2, 13, 50};
     opponent.Init("opponent", attackSet, opponentMaxHealth);
 
     // debug
     GD.Print("oppoonent health: ", opponent.GetHealth());
   }
 
+  public void AITakeTurn() {
+    /* Ensure method is called on opponent's turn */
+
+    if (isPlayerTurn == 1) {
+      GD.Print("AI cannot take a turn on the player's turn");
+      return;
+    }
+
+    /* Choose attack */
+
+    aiAttackChoice = (int)(GD.Randi() % 4); // random number between 0 and 3
+    GD.Print("opponent attack ", aiAttackChoice, " to player. player health remaining: ", player.ReduceHealth(opponent.GetAttackStrength(aiAttackChoice)));
+    isPlayerTurn = 1;
+  }
+
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
   {
-    /* End fight once a beast is defeated */
+    /* End fight once a beast has 0 health */
 
     if (player.GetHealth() <= 0) {
+      GD.Print("opponent defeted player");
       GetTree().ChangeScene("res://Bracket/Bracket.tscn");
     }
     else if (opponent.GetHealth() <= 0) {
+      GD.Print("player defeted opponent");
       GetTree().ChangeScene("res://Bracket/Bracket.tscn");
+    }
+
+    /* AI turn */
+
+    // it seems signals can be recieved in the middle of _Process() so checking health here is necessary
+    if (isPlayerTurn == 0 && opponent.GetHealth() != 0) {
+      AITakeTurn();
     }
   }
 
   /* Signals */
 
   private void _on_B0_pressed() {
+    if (isPlayerTurn == 0) {
+      GD.Print("Cannot attack on opponent's turn.");
+      return;
+    }
     GD.Print("player attack 0 to opponent. opponent health remaining: ", opponent.ReduceHealth(player.GetAttackStrength(0)));
+    isPlayerTurn = 0;
   }
 
   private void _on_B1_pressed() {
+    if (isPlayerTurn == 0) {
+      GD.Print("Cannot attack on opponent's turn.");
+      return;
+    }
     GD.Print("player attack 1 to opponent. opponent health remaining: ", opponent.ReduceHealth(player.GetAttackStrength(1)));
+    isPlayerTurn = 0;
   }
 
   private void _on_B2_pressed() {
+    if (isPlayerTurn == 0) {
+      GD.Print("Cannot attack on opponent's turn.");
+      return;
+    }
     GD.Print("player attack 2 to opponent. opponent health remaining: ", opponent.ReduceHealth(player.GetAttackStrength(2)));
+    isPlayerTurn = 0;
   }
 
   private void _on_B3_pressed() {
+    if (isPlayerTurn == 0) {
+      GD.Print("Cannot attack on opponent's turn.");
+      return;
+    }
     GD.Print("player attack 3 to opponent. opponent health remaining: ", opponent.ReduceHealth(player.GetAttackStrength(3)));
+    isPlayerTurn = 0;
   }
 }
