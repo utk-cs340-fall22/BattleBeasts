adf737f205e11f8fb51e81e5c72f6a5842b3f571
Christopher Canaday <chrislcanaday@gmail.com>
2022-10-18 14:43:52 -0400

    #36 updated getcommits.sh. Now will get commits within the past 2 weeks and also displays the file name

--- getcommits.sh
+++ getcommits.sh
@@ -8,10 +8,16 @@ for i in *; do
             if [[ $dir2 == *.cs ]]; then
 	      if [ $# == 2 ]; then  
 		echo "git blame -w $i/$dir2 | grep -e $1 -e $2 >> ../commits.txt"
-		git blame -w "$dir2" | grep -e $1 -e $2 >> ../commits.txt	
+                if git blame --since=2.weeks -w "$dir2" | grep -q $1; then
+                  echo "\n$dir2\n" >> ../commits.txt
+                fi
+		git blame --since=2.weeks -w "$dir2" | grep -e $1 -e $2 >> ../commits.txt	
       	      else
 		echo "git blame -w $i/$dir2 | grep $1 >> ../commits.txt"
-		git blame -w "$dir2" | grep $1 >> ../commits.txt
+                if git blame --since=2.weeks -w "$dir2" | grep -q $1; then
+                  echo "\n$dir2\n" >> ../commits.txt
+                fi
+		git blame --since=2.weeks -w "$dir2" | grep $1 >> ../commits.txt
       	      fi
             fi
         done
@@ -20,10 +26,16 @@ for i in *; do
     if [[ $i == *.cs ]]; then
       if [ $# == 2 ]; then
         echo "git blame -w $i | grep -e $1 -e $2 >> commits.txt"
-	git blame -w $i | grep -e $1 -e $2 >> commits.txt
+        if git blame --since=2.weeks -w $i | grep -q $1; then
+          echo "\n$i\n" >> commits.txt
+        fi
+	git blame --since=2.weeks -w $i | grep -e $1 -e $2 >> commits.txt
       else
-	echo "git blame -w $i | grep $1 >> commits.txt" 
-	git blame -w $i | grep $1 >> commits.txt
+	echo "git blame -w $i | grep $1 >> commits.txt"
+        if git blame --since=2.weeks -w $i | grep -q $1; then
+          echo "\n$i\n" >> commits.txt 
+	fi
+        git blame --since=2.weeks -w $i | grep $1 >> commits.txt
       fi
     fi
   fi
