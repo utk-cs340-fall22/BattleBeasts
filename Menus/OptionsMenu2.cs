40df2c4211eaac41e20829d3e065b4017f598df3
Christopher Canaday <chrislcanaday@gmail.com>
2022-10-19 17:27:14 -0400

    #33 Added the ability to change the volume in the options menu.

--- Menus/OptionsMenu2.cs
+++ Menus/OptionsMenu2.cs
@@ -4,8 +4,12 @@ using System;
 public class OptionsMenu2 : Control
 {
     bool is_paused;
+    int bus_index;
+    float val;
     private CheckButton fullscreen;
+    private AudioStreamPlayer audio;
     private Node GM;
+    private HSlider vol;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -13,6 +17,9 @@ public class OptionsMenu2 : Control
         this.Hide();
         
         fullscreen = GetNode<CheckButton>("CenterContainer/VBoxContainer/FullscreenButton");
+        audio = GetNode<AudioStreamPlayer>("/root/Gm/Music");
+        vol = GetNode<HSlider>("CenterContainer/VBoxContainer/VolumeSlider");
+        //audio = GetNode("/root/Gm")
         //GM = GetNode<Node>("res://GM");
         
         if(OS.WindowFullscreen){
@@ -20,6 +27,10 @@ public class OptionsMenu2 : Control
         }else{
             fullscreen.Pressed = false;  
         }
+        
+        bus_index = AudioServer.GetBusIndex("Master");
+        val = GD.Db2Linear(AudioServer.GetBusVolumeDb(bus_index));
+        vol.SetValue(val*100);
     }
     
     private void _on_FullscreenButton_pressed()
@@ -36,6 +47,13 @@ public class OptionsMenu2 : Control
     {
         this.Hide();
     }
+    
+    private void _on_VolumeSlider_value_changed(float value)
+    {
+        val = value / 100;
+        AudioServer.SetBusVolumeDb(bus_index,GD.Linear2Db(val));
+    }
+
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
