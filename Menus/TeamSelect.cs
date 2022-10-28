4b640e1126971fc9913f169b96583dd4649d16a6
Colin Canonaco <colin.canonaco@gmail.com>
2022-10-27 21:35:47 -0400

    customization attack options are disabled when already picked and reenabled when unpicked. go button is disabled until all beast/modifier/attack selections have been made

--- Menus/TeamSelect.cs
+++ Menus/TeamSelect.cs
@@ -12,6 +12,7 @@ public class TeamSelect : CanvasLayer
   
   private OptionButton beastSelector, modifierSelector;
   private OptionButton attack0Selector, attack1Selector, attack2Selector, attack3Selector;
+  private BaseButton goButton;
   private Fighter player;
 
   private static Dictionary _beastOptions = null;
@@ -22,7 +23,7 @@ public class TeamSelect : CanvasLayer
   
   Globals g;
   
-  int playerMaxHealth;
+  int playerMaxHealth, attack0Prev, attack1Prev, attack2Prev, attack3Prev;
   int[] attackSet;
   Texture tex;
   
@@ -61,47 +62,98 @@ public class TeamSelect : CanvasLayer
       return _attackOptions;
     }
   }
-  
-  private void InitMenus()
-  {
+
+  private void MakeBeastSelection() {
     int i;
-    Dictionary beast, modifier, attack;
+    Dictionary beast;
 
-    /* Add beast selections */
-
+    beastSelector.AddItem("Beast", 1000);
+    beastSelector.AddSeparator();
     for (i = 0; i < beastOptions.Count; i++) {
       beast = beastOptions[i.ToString()] as Dictionary;
       beastSelector.AddItem((String) beast["name"], i);
-
-      // debug
     }
+    beastSelector.Select(beastSelector.GetItemIndex(1000));
+    beastSelector.SetItemDisabled(beastSelector.GetItemIndex(1000), true);
+  }
 
-    /* Add modifier selections */
+  private void MakeModifierSelection() {
+    int i;
+    Dictionary modifier;
 
+    modifierSelector.AddItem("Modifier", 1000);
+    modifierSelector.AddSeparator();
     for (i = 0; i < modifierOptions.Count; i++) {
       modifier = modifierOptions[i.ToString()] as Dictionary;
       modifierSelector.AddItem((String) modifier["name"], i);
-
-      // debug
     }
-    
-    /* Add attack selections */
+    modifierSelector.Select(modifierSelector.GetItemIndex(1000));
+    modifierSelector.SetItemDisabled(modifierSelector.GetItemIndex(1000), true);
+  }
 
+  private void MakeAttackSelection() {
+    int i;
+    Dictionary attack;
+
+    attack0Selector.AddItem("Attack 1", 1000);
+    attack1Selector.AddItem("Attack 2", 1000);
+    attack2Selector.AddItem("Attack 3", 1000);
+    attack3Selector.AddItem("Attack 4", 1000);
+    attack0Selector.AddSeparator();
+    attack1Selector.AddSeparator();
+    attack2Selector.AddSeparator();
+    attack3Selector.AddSeparator();
     for (i = 0; i < attackOptions.Count; i++) {
       attack = attackOptions[i.ToString()] as Dictionary;
       attack0Selector.AddItem((String) attack["name"], i);
       attack1Selector.AddItem((String) attack["name"], i);
       attack2Selector.AddItem((String) attack["name"], i);
       attack3Selector.AddItem((String) attack["name"], i);
-
-      // debug
     }
+    attack0Selector.Select(attack0Selector.GetItemIndex(1000));
+    attack1Selector.Select(attack1Selector.GetItemIndex(1000));
+    attack2Selector.Select(attack2Selector.GetItemIndex(1000));
+    attack3Selector.Select(attack3Selector.GetItemIndex(1000));
+    attack0Selector.SetItemDisabled(attack0Selector.GetItemIndex(1000), true);
+    attack1Selector.SetItemDisabled(attack1Selector.GetItemIndex(1000), true);
+    attack2Selector.SetItemDisabled(attack2Selector.GetItemIndex(1000), true);
+    attack3Selector.SetItemDisabled(attack3Selector.GetItemIndex(1000), true);
+  }
+
+  // enables the Go button when all selections are valid
+  private void areSelectionsValid() {
+    bool badBeast, badModifier, badAttack0, badAttack1, badAttack2, badAttack3;
+
+    badBeast = beastSelector.IsItemDisabled(beastSelector.GetItemIndex(beastSelector.GetSelectedId()));
+    badModifier = modifierSelector.IsItemDisabled(modifierSelector.GetItemIndex(modifierSelector.GetSelectedId()));
+    badAttack0 = attack0Selector.IsItemDisabled(attack0Selector.GetItemIndex(attack0Selector.GetSelectedId()));
+    badAttack1 = attack1Selector.IsItemDisabled(attack1Selector.GetItemIndex(attack1Selector.GetSelectedId()));
+    badAttack2 = attack2Selector.IsItemDisabled(attack2Selector.GetItemIndex(attack2Selector.GetSelectedId()));
+    badAttack3 = attack3Selector.IsItemDisabled(attack3Selector.GetItemIndex(attack3Selector.GetSelectedId()));
+
+    goButton.Disabled = badBeast || badModifier || badAttack0 || badAttack1 || badAttack2 || badAttack3 ? true : false;
+  }
+  
+  private void InitMenus()
+  {
+    attack0Prev = -1;
+    attack1Prev = -1;
+    attack2Prev = -1;
+    attack3Prev = -1;
+    /* Create selection options */
+
+    MakeBeastSelection();
+    MakeModifierSelection();
+    MakeAttackSelection();
   }
 
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
     g = (Globals)GetNode("/root/Gm");
+
+    goButton = GetNode<BaseButton>("Go");
+    goButton.Disabled = true;
     
     se = g.GetNode<AudioStreamPlayer>("SoundEffects");
     
@@ -120,26 +172,25 @@ public class TeamSelect : CanvasLayer
     player.Init("player", attackSet, playerMaxHealth);
     player.Position = new Vector2(500, 300);
     player.Scale = new Vector2(6, 6);
-    tex = ResourceLoader.Load("res://Assets/Character Sprites/Auril-1.png") as Texture;
-    player.GetNode<Sprite>("Texture").Texture = tex;
 
     // beastSelector.connect("ItemSelected", this, "OnBeastSelected");
     
     InitMenus();
   }
   
-  
-
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
 //      
 //  }
 
+  /* Signals */
   
   private void _on_Beasts_item_selected(int index)
   {    
     Dictionary beast;
+
+    areSelectionsValid();
     
     beast = beastOptions[beastSelector.GetSelectedId().ToString()] as Dictionary;
     tex = ResourceLoader.Load((String)beast["texture"]) as Texture;
@@ -154,30 +205,84 @@ public class TeamSelect : CanvasLayer
   /* These all just play the menu sound */
   private void _on_Modifier_item_selected(int index)
   {
+    areSelectionsValid();
+
     se.Stream = ResourceLoader.Load("res://Assets/Music/MenuClick.tres") as AudioStream;
     se.Play();
   }
   
   private void _on_Attack0_item_selected(int index)
   {
+    areSelectionsValid();
+
+    if (attack3Prev != -1) {
+      attack0Selector.SetItemDisabled(attack0Prev, false);
+      attack1Selector.SetItemDisabled(attack0Prev, false);
+      attack2Selector.SetItemDisabled(attack0Prev, false);
+      attack3Selector.SetItemDisabled(attack0Prev, false);
+    }
+    attack1Selector.SetItemDisabled(index, true);
+    attack2Selector.SetItemDisabled(index, true);
+    attack3Selector.SetItemDisabled(index, true);
+    attack0Prev = index;
+
     se.Stream = ResourceLoader.Load("res://Assets/Music/MenuClick.tres") as AudioStream;
     se.Play();
   }
   
   private void _on_Attack1_item_selected(int index)
   {
+    areSelectionsValid();
+
+    if (attack3Prev != -1) {
+      attack0Selector.SetItemDisabled(attack1Prev, false);
+      attack1Selector.SetItemDisabled(attack1Prev, false);
+      attack2Selector.SetItemDisabled(attack1Prev, false);
+      attack3Selector.SetItemDisabled(attack1Prev, false);
+    }
+    attack0Selector.SetItemDisabled(index, true);
+    attack2Selector.SetItemDisabled(index, true);
+    attack3Selector.SetItemDisabled(index, true);
+    attack1Prev = index;
+
     se.Stream = ResourceLoader.Load("res://Assets/Music/MenuClick.tres") as AudioStream;
     se.Play();
   }
   
   private void _on_Attack2_item_selected(int index)
   {
+    areSelectionsValid();
+
+    if (attack3Prev != -1) {
+      attack0Selector.SetItemDisabled(attack2Prev, false);
+      attack1Selector.SetItemDisabled(attack2Prev, false);
+      attack2Selector.SetItemDisabled(attack2Prev, false);
+      attack3Selector.SetItemDisabled(attack2Prev, false);
+    }
+    attack0Selector.SetItemDisabled(index, true);
+    attack1Selector.SetItemDisabled(index, true);
+    attack3Selector.SetItemDisabled(index, true);
+    attack2Prev = index;
+    
     se.Stream = ResourceLoader.Load("res://Assets/Music/MenuClick.tres") as AudioStream;
     se.Play();
   }
   
   private void _on_Attack3_item_selected(int index)
   {
+    areSelectionsValid();
+
+    if (attack3Prev != -1) {
+      attack0Selector.SetItemDisabled(attack3Prev, false);
+      attack1Selector.SetItemDisabled(attack3Prev, false);
+      attack2Selector.SetItemDisabled(attack3Prev, false);
+      attack3Selector.SetItemDisabled(attack3Prev, false);
+    }
+    attack0Selector.SetItemDisabled(index, true);
+    attack1Selector.SetItemDisabled(index, true);
+    attack2Selector.SetItemDisabled(index, true);
+    attack3Prev = index;
+
     se.Stream = ResourceLoader.Load("res://Assets/Music/MenuClick.tres") as AudioStream;
     se.Play();
   }
