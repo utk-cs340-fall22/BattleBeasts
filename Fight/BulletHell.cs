148d844fe6ff02f8e63c9a66fc0f1edf603d6d39
Christopher Canaday <chrislcanaday@gmail.com>
2022-11-04 20:05:07 -0400

    #69 Minigame now works. Still has some stuff to work on.

--- Fight/BulletHell.cs
+++ Fight/BulletHell.cs
@@ -5,23 +5,69 @@ public class BulletHell : Node2D
 {
     //Area2D bullets = Load("res://Fight/Bullet.tscn");
     public PackedScene Bullet;
+    private int PlayerHealth;
+    private float StartTime;
+    private float GameTime;
+    private float delay;
+    RandomNumberGenerator rng = new RandomNumberGenerator();
+    private Random _random = new Random();
+    PathFollow2D BulletSpawnLocation;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        //Bullet = (PackedScene) ResourceLoader.Load("res:://Fight/Bullet.tscn");
+        PlayerHealth = 100;
+        StartTime = 1;
+        GameTime = 30;
+        delay = (float) .2;
+        //rng.New();
+        rng.Randomize();
+        BulletSpawnLocation = GetNode<PathFollow2D>("BulletSpawn/SpawnLocation");
         Bullet = GD.Load<PackedScene>("res://Fight/Bullet.tscn");
     }
     
     public override void _Process(float delta)
     {
-        var tmp = Bullet.Instance();
-        AddChild(tmp);
+        if(StartTime < 0 && GetChildCount() < 500){
+          if(delay > 0){
+            delay -= delta;
+          }else{
+            _MakeBullet();
+            GD.Print(PlayerHealth);
+            delay = (float) rng.RandfRange((float) .01,(float) .1);
+          }
+        }
+        
+        if(StartTime > 0) StartTime -= delta;
+        if(StartTime < 0) GameTime -= delta;
+        if(PlayerHealth < 0 || GameTime < 0)  _GameOver();
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    
+    private void _GameOver(){
+      QueueFree(); 
+    }
+    
+    private void _MakeBullet()
+    {
+      BulletSpawnLocation.Offset = _random.Next();
+      
+      var BulletInstance = (Area2D) Bullet.Instance();
+      AddChild(BulletInstance);
+      BulletInstance.Position = BulletSpawnLocation.Position;
+      
+      float direction = BulletSpawnLocation.Rotation + Mathf.Pi / 2;
+      
+      direction += RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+      BulletInstance.Rotation = direction;
+    }
+    
+    public void _ChangePlayerHealth(int change)
+    {
+      PlayerHealth += change;  
+    }
+    
+    private float RandRange(float min, float max)
+    {
+        return (float)_random.NextDouble() * (max - min) + min;
+    }
 }
--- Fight/Player.cs
+++ Fight/Player.cs
@@ -1,25 +1,72 @@
 using Godot;
+using Godot.Collections;
 using System;
 
-public class Player : Area2D
+public class Player : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export] public int speed = 200;
+    public Vector2 velocity = new Vector2();
+    Vector2 startpos;
+    private static Dictionary _beastOptions = null;
+    Globals g;
+    Texture playerTexture;
+    
+    
+    private Dictionary beastOptions {
+      get {
+        if (_beastOptions == null) {
+          var file = new File();
+          file.Open("res://Data/Beasts.json", File.ModeFlags.Read);
+          var text = file.GetAsText();
+          _beastOptions = JSON.Parse(text).Result as Dictionary;
+        }
+        return _beastOptions;
+      }
+    }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        Dictionary beast;
+      
+        startpos.x = GetViewport().Size.x/2;
+        startpos.y = GetViewport().Size.y/2;
+        this.Position = startpos;
         
+        g = (Globals)GetNode("/root/Gm");
+        
+        beast = beastOptions[g.playerBeastIndex.ToString()] as Dictionary;
+        playerTexture = ResourceLoader.Load((String)beast["texture"]) as Texture;
+        GetNode<Sprite>("Texture").Texture = playerTexture;
     }
     
-    public override void _Process(float delta)
+    // sets the velocity vector
+    public void GetInput()
     {
-      this.Position = GetViewport().GetMousePosition();
+      velocity = new Vector2();
+
+      if (Input.IsActionPressed("right")){
+        velocity.x += 1;
+      }
+
+      if (Input.IsActionPressed("left")){
+        velocity.x -= 1;
+      }
+
+      if (Input.IsActionPressed("down")){
+        velocity.y += 1;
+      }
+
+      if (Input.IsActionPressed("up")){
+        velocity.y -= 1;
+      }
+      velocity = velocity.Normalized() * speed;
     }
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  {
+    
+    // Moves the character based off character input
+    public override void _PhysicsProcess(float delta)
+    {
+      GetInput();
+      velocity = MoveAndSlide(velocity);
+    }
 }
