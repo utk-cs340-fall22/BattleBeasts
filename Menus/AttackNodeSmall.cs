d72a0b84ed59f5c472d4da378cf0a971e5541302
Christopher Canaday <chrislcanaday@gmail.com>
2022-11-20 18:34:36 -0500

    #95 Added border to the info menu to show what type the move is

--- Menus/AttackNodeSmall.cs
+++ Menus/AttackNodeSmall.cs
@@ -7,6 +7,9 @@ public class AttackNodeSmall : Control
     //private Sprite DmgType;
     private Node Fight;
     private int index;
+    private StyleBoxFlat backgroundstyle;
+    private Panel background;
+    private Color bordercolor;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -19,7 +22,7 @@ public class AttackNodeSmall : Control
       AttackName = GetNode<Label>("AttackName");
       AttackDmg = GetNode<Label>("AttackDmg");
       AttackNum = GetNode<Label>("AttackNum");
-      //DmgType = GetNode<Sprite>("DmgType");
+      background = GetNode<Panel>("Panel");
       Fight = (Node) parent;
       String d = dmg.ToString();
       String n = num.ToString();
@@ -27,6 +30,39 @@ public class AttackNodeSmall : Control
       AttackDmg.Text = "ATK "+ d;
       AttackNum.Text = "HPA " + n;
       index = ind;
+      
+      //background.Panel;
+      backgroundstyle = (StyleBoxFlat) background.GetStylebox("panel");
+      
+      if(type == "Fire"){
+        bordercolor.a = 1;
+        bordercolor.b = (float) .21;
+        bordercolor.g = (float) .21;
+        bordercolor.r = (float) .74;
+      }else if(type == "Electric"){
+        bordercolor.a = 1;
+        bordercolor.b = (float) .21;
+        bordercolor.g = (float) .67;
+        bordercolor.r = (float) .74;
+      }else if(type == "Poison"){
+        bordercolor.a = 1;
+        bordercolor.b = (float) .21;
+        bordercolor.g = (float) .74;
+        bordercolor.r = (float) .35;
+      }else if(type == "Ice"){
+        bordercolor.a = 1;
+        bordercolor.b = (float) .74;
+        bordercolor.g = (float) .74;
+        bordercolor.r = (float) .21;
+      }else if(type == "Dark Magic"){
+        bordercolor.a = 1;
+        bordercolor.b = (float) .74;
+        bordercolor.g = (float) .21;
+        bordercolor.r = (float) .47;
+      }
+      
+      
+      backgroundstyle.BorderColor = bordercolor;
     }
     
     private void _on_TextureButton_pressed()
