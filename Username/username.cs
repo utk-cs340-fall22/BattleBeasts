5b85b44d81b1b4a8086374b10a9f201ca639ed13
Noah Dahle <zrd939@vols.utk.edu>
2022-11-27 16:41:31 -0500

    #88 Gave scene a background

--- Username/username.cs
+++ Username/username.cs
@@ -6,7 +6,7 @@ public class username : Node2D
 
   public override void _Ready()
   {
-
+    GetNode<TextureRect>("TextureRect").Texture = ResourceLoader.Load("res://Assets/lab.png") as Texture;
   }
   
 }
