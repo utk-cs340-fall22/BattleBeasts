72cf78297709a1d3fa34a5f8b548f179ae6830b4
Christopher Canaday <chrislcanaday@gmail.com>
2022-11-29 12:55:50 -0500

    Fixed the getcommits (no longer will print files names that have no code)

--- getcommits.sh
+++ getcommits.sh
@@ -8,13 +8,13 @@ for i in *; do
             if [[ $dir2 == *.cs ]]; then
 	      if [ $# == 2 ]; then  
 		echo "git blame HEAD -w $i/$dir2 | grep -e $1 -e $2 >> ../commits.txt"
-                if git blame HEAD --since=3.weeks -w "$dir2" | grep -q -e $1 -e $2 | sed '/^^/d'; then
+                if git blame HEAD --since=3.weeks -w "$dir2" | sed '/^^/d' | grep -q -e $1 -e $2; then
                   echo -e "\n$dir2\n" >> ../commits.txt
                 fi
 		git blame HEAD --since=3.weeks -w "$dir2" | grep -e $1 -e $2 | sed '/^^/d' >> ../commits.txt	
       	      else
 		echo "git blame HEAD -w $i/$dir2 | grep $1 >> ../commits.txt"
-                if git blame HEAD -b --since=3.weeks -w "$dir2" | grep -q $1 | sed '/^^/d'; then
+                if git blame HEAD -b --since=3.weeks -w "$dir2" | sed '/^^/d' | grep -q $1; then
                   echo -e "\n$dir2\n" >> ../commits.txt
                 fi
 		git blame HEAD --since=3.weeks -w "$dir2" | grep $1 | sed '/^^/d' >> ../commits.txt
@@ -26,13 +26,13 @@ for i in *; do
     if [[ $i == *.cs ]]; then
       if [ $# == 2 ]; then
         echo "git blame -w $i | grep -e $1 -e $2 >> commits.txt"
-        if git blame HEAD --since=3.weeks -w $i | grep -q -e $1 -e $2 | sed '/^^/d'; then
+        if git blame HEAD --since=3.weeks -w $i | sed '/^^/d' | grep -q -e $1 -e $2; then
           echo -e "\n$i\n" >> commits.txt
         fi
 	git blame HEAD --since=3.weeks -w $i | grep -e $1 -e $2 | sed '/^^/d' >> commits.txt
       else
 	echo "git blame -w $i | grep $1 >> commits.txt"
-        if git blame HEAD --since=3.weeks -w $i | grep -q $1 | sed '/^^/d'; then
+        if git blame HEAD --since=3.weeks -w $i | sed '/^^/d' | grep -q $1; then
           echo -e "\n$i\n" >> commits.txt 
 	fi
         git blame HEAD --since=3.weeks -w $i | grep $1 | sed '/^^/d' >> commits.txt
